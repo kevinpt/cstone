af170b3c05fc6adddcba2ac11c329f3d988f0616
Kevin Thibedeau <6765123+kevinpt@users.noreply.github.com>
2022-08-30 18:09:05 -0400

    Move log config to app

--- include/cstone/log_info.h
+++ include/cstone/log_info.h
@@ -1,24 +1,6 @@
 #ifndef LOG_INFO_H
 #define LOG_INFO_H
 
-#include "cstone/platform.h"
-
-// FIXME: MOVE elsewhere
-
-// ******************** Configuration ********************
-#define LOG_TO_RAM  // Force settings to RAM for debug
-
-#if defined LOG_TO_RAM || !defined PLATFORM_EMBEDDED // Small in-memory filesystem for testing
-#  define LOG_NUM_SECTORS 3
-#  define LOG_SECTOR_SIZE 128
-
-#else // Log to flash storage
-// STORAGE0 in STM32 sectors 1-3
-#  define LOG_NUM_SECTORS 3
-#  define LOG_SECTOR_SIZE (16 * 1024)
-#endif
-
-
 
 #ifdef __cplusplus
 extern "C" {
