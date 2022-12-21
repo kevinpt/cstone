e4efa545afbec3ae7c77db13f8178e2541b37c07
Kevin Thibedeau <6765123+kevinpt@users.noreply.github.com>
2022-12-20 22:25:16 -0500

    Add bold macro

--- include/cstone/term_color.h
+++ include/cstone/term_color.h
@@ -1,3 +1,27 @@
+/* SPDX-License-Identifier: MIT
+Copyright 2022 Kevin Thibedeau
+(kevin 'period' thibedeau 'at' gmail 'punto' com)
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice (including the next
+paragraph) shall be included in all copies or substantial portions of the
+Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+
 #ifndef TERM_COLOR_H
 #define TERM_COLOR_H
 
@@ -11,6 +35,9 @@
 #define A_MAG "\033[0;35m"
 #define A_CYN "\033[0;36m"
 #define A_WHT "\033[0;37m"
+
+
+#define A_BOLD "\033[1m"
 
 #define A_BBLK "\033[1;30m"
 #define A_BRED "\033[1;31m"
